92d17fb9e456d7034010b102d6825d90d2a24ede
Makenna Garrett <kenna.dunford@gmail.com>
2021-04-27 14:58:25 -0600

    debugged routes and got app rendering in browser.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {HashRouter} from 'react-router-dom';
+const Router = HashRouter
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router >
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
