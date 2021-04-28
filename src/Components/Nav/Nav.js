c8fcf86f0ed7aa133cd875b8ca408643d8df1028
Makenna Garrett <kenna.dunford@gmail.com>
2021-04-28 16:22:59 -0600

    Added dropdown functionality

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,18 +1,27 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
+import './Nav.scss';
 
 const Nav = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errorMsg, setErrorMsg] = useState('');
     const [email, setEmail] = useState('');
-    const [profilePic, setProfilePic] = useState('');
+    const [authMenu, setAuthMenu] = useState('authClosed');
+    const [loginMenu, setLoginMenu] = useState('loginClosed');
+    const [registerMenu, setRegisterMenu] = useState('registerClosed');
+    const [menu, setMenu] = useState('closed');
 
+    const {user_id} = false;
     function login(){
         axios.post('/api/login', {username, password})
             .then(res => {
                 const {user_id, username} = res.data;
                 //Update redux
+                props.updateUser({username, user_id})
+                console.log(props)
+                setLoginMenu('loginClosed')
             })
             .catch(err => {
                 console.log(err)
@@ -21,10 +30,13 @@ const Nav = (props) => {
     };
         
     function register(){
+        const profilePic = `https://robohash.org/${username}.png`;
         axios.post('/api/register', {username, password, email, profilePic})
             .then(res => {
                 const {user_id} = res.data;
                 //Update redux
+                props.updateUser({username, user_id})
+                setRegisterMenu('registerClosed')
             })
             .catch(err => {
                 console.log(err)
@@ -39,40 +51,85 @@ const Nav = (props) => {
                 setPassword('');
                 setErrorMsg('');
                 setEmail('');
-                setProfilePic('');
                 //Update Redux
-
+                props.logout()
                 props.history.push('/');
+                handleClick();
             })
     };
+
+    function handleClick(){
+        setLoginMenu('loginClosed');
+        setRegisterMenu('registerClosed');
+        if(!user_id){
+            if(authMenu === 'authClosed'){
+                setAuthMenu('authOpen');              
+            } else {
+                setAuthMenu('authClosed');
+            };
+            console.log('clicked!')
+        } else {
+            if(menu === 'closed'){
+                setMenu('open');
+            } else {
+                setMenu('closed');
+            };
+        };
+    };
+
+    function registerView(){
+        setAuthMenu('authClosed');
+        setMenu('closed');
+        if(registerMenu === 'registerClosed'){
+            setRegisterMenu('registerOpen');
+        }
+    };
+
+    function loginView(){
+        setAuthMenu('authClosed');
+        setMenu('closed');
+        if(loginMenu === 'loginClosed'){
+            setLoginMenu('loginOpen')
+        }
+    };
+
+    function removeErrorMsg(){
+        setUsername('')
+        setPassword('')
+        setErrorMsg('')
+      };
 
     return(
         <div>
             <div>logo</div>
             <div className='profilePic'>
-                <img src={profilePic} alt='profile pic' />
+                <img src={`https://robohash.org/${username}.png`} alt='profile pic' onClick={handleClick} />
             </div>
-            <ul >
-                <li>Sign Up</li>
-                <li>Login</li>
+
+            <ul className={authMenu === 'authClosed' ? 'closed' : 'open'}>
+                <li onClick={registerView}>Sign Up</li>
+                <li onClick={loginView}>Login</li>
             </ul>
-            <div >
-                <h1>{errorMsg} X</h1>
-                <input placeholder='Username'/>
-                <input placeholder='Password'/>
+
+            <div className={loginMenu === 'loginClosed' ? 'closed' : 'open'}>
+                {errorMsg && <h3>{errorMsg}<span onClick= {removeErrorMsg}>X</span></h3>}
+                <input placeholder='Username' onChange={e => setUsername(e.target.value)}/>
+                <input placeholder='Password' type='password' onChange={e => setPassword (e.target.value)}/>
                 <button onClick={login}>Login</button>
             </div>
-            <div >
-                <h1>{errorMsg} X</h1>
-                <input placeholder='Username'/>
-                <input placeholder='Email' />
-                <input placeholder='Password'/>
+
+            <div className={registerMenu === 'registerClosed' ? 'closed' : 'open'}>
+                {errorMsg && <h3>{errorMsg}<span onClick= {removeErrorMsg}>X</span></h3>}
+                <input placeholder='Username' onChange={e => setUsername(e.target.value)}/>
+                <input placeholder='Email' onChange={e => setEmail(e.target.value)}/>
+                <input placeholder='Password' onChange={e => setPassword (e.target.value)}/>
                 <button onClick={register}>Register</button>
             </div>
-            <ul>
-                <li>Your Trips</li>
-                <li>Account</li>
-                <li>Bucket List</li>
+
+            <ul className={menu === 'closed' ? 'closed' : 'open'}>
+                <Link to={`/all-trips/${user_id}`} ><li>Your Trips</li></Link>
+                <Link><li>Account</li></Link> 
+                <Link><li>Bucket List</li></Link>
                 <li onClick={logout}>Logout</li>
             </ul>
         </div>
