1883ebebad222ff2c1c884819a113425e9818f0e
Makenna Garrett <kenna.dunford@gmail.com>
2021-04-28 16:25:46 -0600

    updating index.js to handle store and installs needed for redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
 const Router = HashRouter
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router >
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Provider  store={store}>
+    <React.StrictMode>
+      <Router >
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById('root')
 );
 
