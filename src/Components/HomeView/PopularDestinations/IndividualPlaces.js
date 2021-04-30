df7eff6fc0e1e7a463a6689dc5f8f996cc641d8d
Kaya Stokes <kayatabitha@gmail.com>
2021-04-30 12:07:44 -0600

    link to search page from individual places

--- src/Components/HomeView/PopularDestinations/IndividualPlaces.js
+++ src/Components/HomeView/PopularDestinations/IndividualPlaces.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './IndividualPlaces.scss';
 
 const IndividualPlaces = (props) => {
 
   return(
     <div className='mapped-place'>
+        <Link to='/search-page'>
       <div>{props.places.place}</div>
       <img className='mapped-image' src={props.places.url} alt={props.places.place} />
+        </Link>
     </div>
   )
 }
