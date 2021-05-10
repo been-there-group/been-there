6058bb325f532e5176221fb06f028f2c30431460
Makenna Garrett <kenna.dunford@gmail.com>
2021-05-10 12:47:39 -0600

    started work on React Date Picker

--- src/Components/HomeView/Explore/ExploreResults/ExploreResults.js
+++ src/Components/HomeView/Explore/ExploreResults/ExploreResults.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 import ExploreResultsDisplay from './ExploreResultsDisplay';
+import Nav from '../../../Nav/Nav';
+import './ExploreResults.scss';
 const key = process.env.REACT_APP_GOOGLE_API;
 
 // import explore from '../../../explore';
@@ -13,32 +15,34 @@ const ExploreResults = (props) => {
     useEffect(() => {
 
       const activity = (props.match.params.name).toLowerCase()
-    axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${activity}&key=${key}`)
-    .then(res => {
-      let first = [...res.data.results];
-      setPlaces(first)
-      if (res.data.next_page_token) {
-          setTimeout(() => {
-           return axios
-              .get(
-                `https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${res.data.next_page_token}&key=${key}`
-                )
-              .then((res) => {
-                setPlaces([...first, ...res.data.results]);
-              });
+      axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${activity}&key=${key}`)
+        .then(res => {
+          let first = [...res.data.results];
+          setPlaces(first)
+          if (res.data.next_page_token) {
+            setTimeout(() => {
+              return axios
+                .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${res.data.next_page_token}&key=${key}`)
+                .then((res) => {
+                  setPlaces([...first, ...res.data.results]);
+                });
             }, 3000);
           }
         })
-      }, [])
+    }, [])
     return(
         <div>
-          {places.map((places, index) => {
-            return (
-              <div>
-                <ExploreResultsDisplay key={index} places={places}/>
-              </div>
-            );
-          })}
+          <Nav />
+          <div className='mappedExplore'>
+            <header>These are mapped explore</header>
+            {places.map((places, index) => {
+              return (
+                <div>
+                  <ExploreResultsDisplay key={index} places={places}/>
+                </div>
+              );
+            })}
+          </div>
         </div>
     )
 }
