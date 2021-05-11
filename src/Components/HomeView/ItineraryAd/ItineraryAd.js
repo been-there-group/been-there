7aba2fbf1daf3c35fd721e1c676a73725e6d10bd
Makenna Garrett <kenna.dunford@gmail.com>
2021-05-11 11:31:01 -0600

    fixed bug of the trips page and worked on styling for that page. Aslo removed console.logs from app to help clear out the console.

--- src/Components/HomeView/ItineraryAd/ItineraryAd.js
+++ src/Components/HomeView/ItineraryAd/ItineraryAd.js
@@ -7,7 +7,7 @@ const ItineraryAd = () => {
         <section className='itinerary'>
             <div className='itineraryWords'>
                 <header className='itineraryHeader'>Checkout our new Itinerary feature!</header>
-                <p className='itineraryText'> Plan your trip one day at a time using our <div className='myTripAd'>&#8827;My Trip&#8826;</div> feature. Assign activities to each day of your trip to keep your vacation organized so you can focus on having fun.</p>
+                <div className='itineraryText'> Plan your trip one day at a time using our <p className='myTripAd'>&#8827;My Trip&#8826;</p> feature. Assign activities to each day of your trip to keep your vacation organized so you can focus on having fun.</div>
             </div>
             <img className='ad-image' src='https://images.unsplash.com/photo-1513128034602-7814ccaddd4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhbm5lcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60' />
         </section>
